3cbe993cda02427e739e66da8febe09516b60cef
lcontrerasl <l.contrerasl@alumnos.duoc.cl>
2016-07-01 01:22:10 -0300

    actualizacion final

--- sistema/accform/ingresoProducto.php
+++ sistema/accform/ingresoProducto.php
@@ -2,7 +2,33 @@
 
 $conexion = mysql_connect('localhost', 'root', '');
 mysql_select_db('farm', $conexion);
-$sql = 'INSERT INTO producto(nombreProducto, precio_refProducto, stockProducto ) VALUE(\'' . $_POST['nombreProducto'] . '\',\'' . $_POST['precio_refProducto'] . '\',\'' . $_POST['stockProducto'] . '\')';
+$sql = 'INSERT INTO producto('
+        . ' nombreProducto,'
+        . ' precio_refProducto,'
+        . ' stockProducto,'
+        . ' tipo_producto_idTipo_producto,'
+        . ' unidad_idUnidad'
+        . ' ) '
+        . 'VALUE('
+        . '\'' . $_POST['nombreProducto'] . '\','
+        . '\'' . $_POST['precio_refProducto'] . '\','
+        . '\'' . $_POST['stockProducto'] . '\','
+        . '\'' . $_POST['tipo_producto_idTipo_producto'] . '\','
+        . '\'' . $_POST['unidad_idUnidad'] . '\''
+        . ')';
 mysql_query($sql);
-header('Location: http://localhost/farm/sistema/listadoProductos.php');
+
+$sql = 'SELECT * FROM producto ORDER BY idProducto DESC LIMIT 1';
+mysql_query($sql);
+
+$result = mysql_query($sql);
+$productoAux;
+if ($row = mysql_fetch_array($result)) {
+    do {
+        $productoAux = $row;
+    } while ($row = mysql_fetch_array($result));
+}
+
+
+header('Location: http://localhost/farm/sistema/editarProducto.php?idProducto=' . $productoAux['idProducto']);
 die();
