51e7502f7a9cfb9dba737a12ed40dbe062ee124b
lcontrerasl <l.contrerasl@alumnos.duoc.cl>
2016-07-01 02:47:42 -0300

    Ultimo detalle....

--- componentes/header.php
+++ componentes/header.php
@@ -34,7 +34,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php"><img src="images/logo_farm_ok.png" alt="logo"></a>
+                <a class="navbar-brand" href="index.php"><img src="images/logo_farm_ok.png" alt="logo-farm"></a>
             </div>
 
             <div class="collapse navbar-collapse navbar-right">
